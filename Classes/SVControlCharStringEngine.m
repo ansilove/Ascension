2b8dad2412755bd79758ed6d4b91593e909ecf45
ByteProject <stefan.vogt@byteproject.net>
2011-08-02 23:48:13 +0200

    modified all headers to refer to the new MIT-style license

--- Classes/SVControlCharStringEngine.m
+++ Classes/SVControlCharStringEngine.m
@@ -2,9 +2,11 @@
 //  SVControlCharStringEngine.m
 //  Ascension
 //
-//  Coded by Stefan Vogt.
-//  Released under the FreeBSD license.
-//  http://www.byteproject.net
+//  Copyright (c) 2011, Stefan Vogt. All rights reserved.
+//  http://byteproject.net
+//
+//  Use of this source code is governed by a MIT-style license.
+//  See the file LICENSE for details.
 //
 
 #import "SVControlCharStringEngine.h"
