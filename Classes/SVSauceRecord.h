9d7a0b8b8792ef99ccca6e43a82ad6cdbe4197dc
ByteProject <stefan.vogt@byteproject.net>
2011-12-17 03:49:48 +0100

    class for allocating SAUCE records
    not much to see yet as this is an initial commit!

---
+++ Classes/SVSauceRecord.h
@@ -0,0 +1,16 @@
+//
+//  SVSauceRecord.h
+//  Ascension
+//
+//  Copyright (c) 2010-2011, Stefan Vogt. All rights reserved.
+//  http://byteproject.net
+//
+//  Use of this source code is governed by a MIT-style license.
+//  See the file LICENSE for details.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVSauceRecord : NSObject
+
+@end
---
+++ Classes/SVSauceRecord.m
@@ -0,0 +1,16 @@
+//
+//  SVSauceRecord.m
+//  Ascension
+//
+//  Copyright (c) 2010-2011, Stefan Vogt. All rights reserved.
+//  http://byteproject.net
+//
+//  Use of this source code is governed by a MIT-style license.
+//  See the file LICENSE for details.
+//
+
+#import "SVSauceRecord.h"
+
+@implementation SVSauceRecord
+
+@end
