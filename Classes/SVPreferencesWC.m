7b4a6eedd5f66211ecdf4f938693f5fb897981de
ByteProject <stefan.vogt@byteproject.net>
2012-03-25 03:11:34 +0200

    after a while you realize... time flies!

--- Classes/SVPreferencesWC.m
+++ Classes/SVPreferencesWC.m
@@ -2,7 +2,7 @@
 //  SVPreferencesWC.m
 //  Ascension
 //
-//  Copyright (c) 2010-2011, Stefan Vogt. All rights reserved.
+//  Copyright (c) 2010-2012, Stefan Vogt. All rights reserved.
 //  http://byteproject.net
 //
 //  Use of this source code is governed by a MIT-style license.
--- main.m
+++ main.m
@@ -2,9 +2,11 @@
 //  main.m
 //  Ascension
 //
-//  Coded by Stefan Vogt, created May 25, 2010.
-//  Released under the FreeBSD license.
-//  http://www.byteproject.net
+//  Copyright (c) 2010-2012, Stefan Vogt. All rights reserved.
+//  http://byteproject.net
+//
+//  Use of this source code is governed by a MIT-style license.
+//  See the file LICENSE for details.
 //
 
 #import <Cocoa/Cocoa.h>
